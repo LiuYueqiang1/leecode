4a8138ea21af1b336af5e625811c10d05ae654fa
LiuYueqiang1 <122972454+LiuYueqiang1@users.noreply.github.com>
2023-06-09 16:55:21 +0800

    209.长度最小的子数组 双指针法 思路：只需要讲数组中的值累加与目标值比较即可，不用创建一个新数组

--- array/209_minSubArrayLen/main.go
+++ array/209_minSubArrayLen/main.go
@@ -5,6 +5,7 @@ import "fmt"
 // 给定一个含有 n 个正整数的数组和一个正整数 s ，
 // 找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，并返回其长度。如果不存在符合条件的子数组，返回 0。
 // 输入：s = 7, nums = [2,3,1,2,4,3] 输出：2 解释：子数组 [4,3] 是该条件下的长度最小的子数组。
+
 func minSubArrayLen(s int, nums []int) int {
 	minLen := len(nums) + 1 // 最小长度
 	left, sum := 0, 0       // 左指针和子数组和
@@ -28,6 +29,7 @@ func min(x, y int) int {
 	}
 	return y
 }
+
 func main() {
 	res := minSubArrayLen(7, []int{2, 3, 1, 2, 4, 3})
 	fmt.Println(res)
