2d67a2e980cfc34e43ebfa7e5e0b2c59fb497c28
lyq <lyq754294544@gmail.com>
2023-06-11 21:36:16 +0800

    review 数组

--- array/704_binarysearch/main.go
+++ array/704_binarysearch/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+//写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
 // 左闭右闭区间
 func errsearch(nums []int, target int) int {
 	left := 0
