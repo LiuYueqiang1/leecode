9eb7fcdfaf93b8786fb9cf74212cdf0fa9ccce8f
LiuYueqiang1 <122972454+LiuYueqiang1@users.noreply.github.com>
2023-06-28 10:28:20 +0800

    20.左右括号匹配 哈希和栈

--- stack/20_isvalid/main.go
+++ stack/20_isvalid/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 // 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
 //
@@ -16,35 +14,55 @@ import (
 // 1、左边多余了 { { { ( ) } }
 // 2、中间不匹配
 // 3、右边多余了
-type stack struct {
-	TopStack    int
-	BottomStack int
-	ValidArr    []int32
-}
-
 func isValid(s string) bool {
-	st := stack{
-		TopStack:    -1,
-		BottomStack: 0,
-		ValidArr:    make([]int32, 0),
+	hash := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
 	}
-	for _, v := range s {
-		if v == 123 || v == 91 || v == 40 {
-			st.ValidArr = append(st.ValidArr, v)
-			st.TopStack++
-		} else if v == 41 || v == 93 || v == 125 {
+	stack := make([]byte, 0)
+	if s == "" {
+		return true
+	}
 
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[s[i]] { // 条件3 条件2
+			// 只要是匹配，栈的长度一定>0 且在这个else if 执行
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
 		}
 	}
-
-	return true
+	return len(stack) == 0 //条件 1  如果最后栈没有清空的话
 }
-
-func main() {
-	a := "{[()]}"
-	//123 91 40 41 93 125
-	for _, v := range a {
-		fmt.Printf(" %d", v)
+func isValid2(s string) bool {
+	hash := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
 	}
 
+	stack := make([]rune, 0)
+	for _, v := range s {
+		if v == '(' || v == '[' || v == '{' {
+			stack = append(stack, v)
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[v] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
+func main() {
+	a := "{{[()]}"
+	//123 91 40 41 93 125
+	fmt.Println(a[1]) //91
+	fmt.Println(')')
+	s := []int{1, 2, 3, 4}
+	fmt.Println(s[1]) //切片中的第几个数
+	q := isValid(a)
+	fmt.Println(q)
 }
