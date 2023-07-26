cff4e79039562e52b69c4eb04bfc12d29f3cb7f5
LiuYueqiang1 <122972454+LiuYueqiang1@users.noreply.github.com>
2023-07-26 17:27:33 +0800

    144.二叉树前序遍历

--- binarytree/144_preorderTraversal/main.go
+++ binarytree/144_preorderTraversal/main.go
@@ -1,27 +1,33 @@
 package main
 
-import "container/list"
+// 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
 func preorderTraversal(root *TreeNode) []int {
-	ans := []int{}
-
-	if root == nil {
-		return ans
-	}
-
-	st := list.New()
-	st.PushBack(root)
-
-	for st.Len() > 0 {
-		node := st.Remove(st.Back()).(*TreeNode)
-
-		ans = append(ans, node.Val)
-		if node.Right != nil {
-			st.PushBack(node.Right)
-		}
-		if node.Left != nil {
-			st.PushBack(node.Left)
+	var res = make([]int, 0)
+	var traversal func(node *TreeNode)
+	// 定义一个函数
+	traversal = func(node *TreeNode) {
+		if node != nil {
+			res = append(res, node.Val)
+			traversal(node.Left)
+			traversal(node.Right)
 		}
 	}
-	return ans
+
+	traversal(root)
+	return res
 }
