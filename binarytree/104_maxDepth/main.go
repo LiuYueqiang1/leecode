954944b02636b043a1793b0f11fecea03225406c
LiuYueqiang1 <122972454+LiuYueqiang1@users.noreply.github.com>
2023-09-12 21:56:20 +0800

    104、二叉树的最大深度 前序遍历

---
+++ binarytree/104_maxDepth/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// 二叉树的最大深度 前序遍历
+// 二叉树的最大高度 后序遍历
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+func main() {
+	// 创建一个示例二叉树
+	root := &TreeNode{Val: 1}
+	root.Left = &TreeNode{Val: 2}
+	root.Right = &TreeNode{Val: 2}
+	root.Left.Left = &TreeNode{Val: 3}
+	root.Left.Right = &TreeNode{Val: 4}
+	root.Right.Left = &TreeNode{Val: 4}
+	root.Right.Right = &TreeNode{Val: 3}
+
+	fmt.Println(maxDepth(root))
+}
